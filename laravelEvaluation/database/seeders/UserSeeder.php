d365dbd7b098f27f5ac477fddee80243b4d1e8db
Dxnnnn <dhexny18@gmail.com>
2025-09-29 10:30:41 +0800

    update

--- laravelEvaluation/database/seeders/UserSeeder.php
+++ laravelEvaluation/database/seeders/UserSeeder.php
@@ -10,10 +10,30 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        // Create Admin User
         User::create([
-            'name' => 'Admin',
-            'email' => 'Admin@gmail.com',
-            'password' => Hash::make('test1'),
+            'name' => 'Administrator',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin123'),
+            'role' => 'admin',
+            'email_verified_at' => now(),
+        ]);
+
+        // Create Student User
+        User::create([  
+            'name' => 'Student',
+            'email' => 'Dixni@gmail.com',
+            'password' => Hash::make('user1'),
+            'role' => 'student',
+            'email_verified_at' => now(),
+        ]);
+
+        // Create another student for testing
+        User::create([
+            'name' => 'Student',
+            'email' => 'student@example.com',
+            'password' => Hash::make('student123'),
+            'role' => 'student',
             'email_verified_at' => now(),
         ]);
     }
